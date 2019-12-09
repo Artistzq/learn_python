7757d3923f48634a44d35b24a9f1020d4db80479
Azq <2237065231@qq.com>
2019-12-09 22:10:29 +0800

    八数码 完成

--- AStar/solution.py
+++ AStar/solution.py
@@ -16,6 +16,7 @@ class Solution(object):
     def __init__(self, start_state_array, end_state_array=State.CORRECT_ARRAY):
         # 开始状态节点
         self.start_state = State(start_state_array)  # type: State
+        # self.start_state.father = State(State.INIT_ARRAY)
         # 终止状态节点
         self.end_state = State(end_state_array)  # type: State
         # 当前状态节点
@@ -28,9 +29,18 @@ class Solution(object):
         self.count = 0
         # 搜索的时间
         self.solve_time = 0
-        self.result_state = False
+        # 是否可解
+        self.is_solvable = False
+        # 是否有解
+        self.has_solution = False
 
-    def solve(self, method = 1):
+    def judge_solvable(self):
+        a = self.start_state.get_inverse_num()
+        b = self.end_state.get_inverse_num()
+        if a % 2 == b % 2:
+            self.is_solvable = True
+
+    def solve(self, method=2):
         """
         解此八数码问题
         Args:
@@ -38,45 +48,60 @@ class Solution(object):
         Returns:
 
         """
-        start_time = time.perf_counter()
-        self.open_list.append(self.start_state)
-        while self.open_list:
-            self.current_state = self.open_list.pop()
-            if self.current_state.array_equal_to(self.end_state):
-                end_time = time.perf_counter()
-                self.solve_time = end_time - start_time
-                self.result_state = True
-                return True
-            # 8位数码状态均可扩展出子状态，所以生成所有的子状态
-            child_list = Operation.get_all_next_state(self.current_state)
-            for child_state in child_list:  # type: State
-                exist_open, same_state_open = child_state.array_is_exist_in(self.open_list)
-                exist_close, same_state_close = child_state.array_is_exist_in(self.close_list)
-                if not (exist_open or exist_close):
-                    child_state.calculate_f(self.end_state, method)
-                    # child_list.remove(child_state)
-                    self.open_list.append(child_state)
-                elif exist_open:
-                    if child_state.f < same_state_open.f:
-                        same_state_open.set_father(child_state.father)
-                        same_state_open.calculate_f(self.end_state, method)
-                elif exist_close:
-                    if child_state.f < same_state_close.f:
-                        self.open_list.append(same_state_close)
-                        self.close_list.remove(same_state_close)
-                        same_state_close.calculate_f()
-            self.close_list.append(self.current_state)
-            self.open_list.sort(key=attrgetter("f"), reverse=True)
-            self.count += 1
-            # if self.count % 500 == 0:
-            #     print("检查了", self.count, "个结点")
-            #     print("open 表长：", len(self.open_list))
-            #     print("close 表长：", len(self.close_list))
-            #     print()
-        return False
+        self.judge_solvable()
+        # 如果是可解的
+        if self.is_solvable:
+            start_time = time.perf_counter()
+            self.open_list.append(self.start_state)
+            self.start_state.calculate_f(self.end_state, method)
+            while self.open_list:
+                sss = time.clock()
+                self.current_state = self.open_list.pop()
+                self.count += 1
+                if self.current_state.array_equal_to(self.end_state):
+                    end_time = time.perf_counter()
+                    self.solve_time = end_time - start_time
+                    self.has_solution = True
+                    return
+                # 8位数码状态均可扩展出子状态，所以生成所有的子状态
+                child_list = Operation.get_all_next_state(self.current_state)  # type: List[State]
+                for child_state in child_list:  # type: State
+                    in_open_list, same_state_open = child_state.array_is_exist_in(self.open_list)
+                    in_close_list, same_state_close = child_state.array_is_exist_in(self.close_list)
+                    if not (in_open_list or in_close_list):
+                        child_state.calculate_f(self.end_state, method)
+                        self.open_list.append(child_state)
+                    elif in_open_list:
+                        if child_state.f < same_state_open.f:
+                            child_state.calculate_f(self.end_state, method)
+                            same_state_open.update_info(child_state)
+                    elif in_close_list:
+                        if child_state.f < same_state_close.f:
+                            child_state.calculate_f(self.end_state, method)
+                            same_state_close.update_info(child_state)
+                            self.open_list.append(same_state_close)
+                            self.close_list.remove(same_state_close)
+                self.close_list.append(self.current_state)
+                self.open_list.sort(key=attrgetter("f"), reverse=True)
+                if self.count % 500 == 0:
+                    print("搜索了", self.count, "个结点")
+                    print("open 表长：", len(self.open_list))
+                    print("close 表长：", len(self.close_list))
+                    print()
+                rrr = time.clock()
+                ttt = rrr - sss
+                print(ttt)
+            # open表空也没找到
+            print("异常：open表空未搜索到解")
+            return
 
     def display_solution(self):
-        if self.result_state == True:
+        print("初始状态：")
+        self.start_state.display_array()
+        print("目的状态：")
+        self.end_state.display_array()
+        print("=============")
+        if self.is_solvable and self.has_solution:
             cur_state = self.current_state
             path = []
             while cur_state is not None:
@@ -84,9 +109,13 @@ class Solution(object):
                 cur_state = cur_state.father
             path.reverse()
             for state in path:
+                print(state.g)
                 state.display_array()
             print("共检查了", self.count, "个结点")
+            print("从初始状态到目的状态经历了", len(path) - 1, "次操作")
             print("检索时间：", self.solve_time)
+        else:
+            print("该初始状态和结束状态无法解")
 
     def reset(self):
         """重置解算法"""
@@ -97,16 +126,68 @@ class Solution(object):
         # 搜索的结点数
         self.count = 0
 
+
 if __name__ == "__main__":
     start_array = np.array([[1, 2, 3], [4, 7, 5], [6, 0, 8]])
-    test = [State.CORRECT_ARRAY,
-            np.array([[2, 8, 3], [1, 6, 4], [7, 0, 5]]),
-            np.array([[1, 2, 3], [8, 0, 4], [7, 6, 5]]), np.array([[1, 2, 3], [8, 4, 5], [7, 6, 0]]),
-            np.array([[1, 2, 3], [8, 4, 5], [7, 0, 6]]), np.array([[1, 2, 0], [3, 4, 5], [6, 7, 8]]),
-            np.array([[1, 2, 3], [0, 4, 5], [6, 8, 7]])]
+    test = [
+        # 书上示例 0，1
+        np.array([[2, 8, 3],
+                  [1, 6, 4], # 0
+                  [7, 0, 5]]),
+        np.array([[1, 2, 3],
+                  [8, 0, 4], # 1
+                  [7, 6, 5]]),
+        np.array([[1, 0, 3],
+                  [7, 2, 4], # 和1搭配 2
+                  [6, 8, 5]]),
+        np.array([[1, 2, 3],
+                  [8, 4, 5],
+                  [7, 0, 6]]),
+        # 和 correctArray搭配使用
+        np.array([[1, 2, 0],
+                  [3, 4, 5], # 4
+                  [6, 7, 8]]),
+        np.array([[1, 2, 3],
+                  [0, 4, 5],
+                  [6, 8, 7]]),
+        np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [7, 8, 0]]),
+        np.array([[1, 2, 3],
+                  [4, 5, 6],
+                  [8, 7, 0]]),
+        # 最大反序 8, 9
+        np.array([[8, 7, 6],
+                  [5, 0, 4],  # 8
+                  [3, 2, 1]]),
+        np.array([[1, 2, 3],
+                  [4, 0, 5],  # 9
+                  [6, 7, 8]]),
+        # 无解
+        np.array([[2, 5, 4],
+                  [3, 0, 7],  # 10
+                  [1, 8, 6]]),
+        np.array([[1, 2, 3],
+                  [8, 0, 4],  # 11
+                  [7, 6, 5]]),
+        # 解二
+        np.array([[5, 4, 1],
+                  [6, 7, 0],  # 12
+                  [2, 8, 3]]),
+        np.array([[1, 2, 3],
+                  [4, 5, 6],  # 13
+                  [7, 8, 0]]),
+        # 解三
+        np.array([[5, 4, 1],
+                  [6, 7, 0],  # 14
+                  [2, 8, 3]]),
+        np.array([[5, 4, 1],
+                  [6, 7, 3],  # 15
+                  [2, 8, 0]]),
+        State.CORRECT_ARRAY
+    ]
 
-    solution = Solution(test[0], State.CORRECT_ARRAY)
-
-    start = time.perf_counter()
-    solution.solve()
+    solution = Solution(test[8], test[9])
+    print("解：")
+    solution.solve(2)
     solution.display_solution()
--- AStar/state.py
+++ AStar/state.py
@@ -3,6 +3,15 @@
 
 import numpy as np
 from typing import List, Optional
+from enum import Enum
+
+
+class Direction(Enum):
+    NO_MOVE = 0
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
 
 
 class State(object):
@@ -11,8 +20,10 @@ class State(object):
     """
 
     ORDER = 3  # type: int
-    ERROR_ARRAY = np.zeros((3, 3), np.int8)  # type: np.ndarray
+    ZERO_ARRAY = np.zeros((3, 3), np.int8)  # type: np.ndarray
+    ERROR_ARRAY = np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]])
     CORRECT_ARRAY = np.array([[1, 2, 3], [4, 0, 5], [6, 7, 8]])  # type: np.ndarray
+    INIT_ARRAY = np.zeros((3, 3), np.int8)  # type: np.ndarray
 
     def __init__(self, state_array: np.ndarray, f=0, g=0, h=0):
         """
@@ -35,10 +46,25 @@ class State(object):
         self.h = h
         # 父状态结点
         self.father = None  # type: (State, Optional)
+        # 上一次从什么方向移动而得到
+        self.last_direction_from = Direction.NO_MOVE
 
     def set_g(self, g):
         self.g = g
         self.f = self.g + self.h
+
+    def update_info(self, new_state):
+        """
+        根据新状态(数码格局相同)，更新自己的价值信息
+        Args:
+            new_state (State): 新状态
+        Returns:
+        """
+        self.f = new_state.f
+        self.g = new_state.g
+        self.h = new_state.h
+        self.father = new_state.father
+        self.last_direction_from = new_state.last_direction_from
 
     def set_father(self, father):
         """
@@ -61,9 +87,37 @@ class State(object):
             if x != y:
                 count += 1
         self.h = count
-        self.f = self.g + self.h
 
-    def calculate_f(self, end_state, method = 1):
+    def calculate_h_2(self, end_state):
+        """
+        计算到end_state的估计代价
+        方法2：估价函数是该状态格局域目的格局位置的曼哈顿距离
+        Args:
+            end_state (State):
+        """
+        dist = 0
+        for index_self_x in range(State.ORDER):
+            for index_self_y in range(State.ORDER):
+                temp = np.where(end_state.state_array == self.state_array[index_self_x][index_self_y])
+                index_end = [temp[0][0], temp[1][0]]
+                dist += abs(index_end[0] - index_self_x) + abs(index_end[1] - index_self_y)
+        self.h = dist
+
+    def calculate_h_3(self, end_state):
+        count_1 = 0
+        for x, y in zip(np.nditer(self.state_array), np.nditer(end_state.state_array)):
+            if x != y:
+                count_1 += 1
+        count_2 = 0
+        xy = list(self.state_array.flatten().tolist())
+        xy.remove(0)
+        for x in range(len(xy)):
+            for y in range(x, len(xy)):
+                if xy[x] > xy[y]:
+                    count_2 += 1
+        self.h = count_1 + count_2
+
+    def calculate_f(self, end_state, method=1):
         """
         Args:
             method (int):
@@ -71,12 +125,22 @@ class State(object):
         """
         if method == 1:
             self.calculate_h_1(end_state)
+            self.f = self.g + self.h
+        elif method == 2:
+            self.calculate_h_2(end_state)
+            self.f = self.g + self.h
+        elif method == 3:
+            self.calculate_h_3(end_state)
+            self.f = self.g + self.h
 
     def array_equal_to(self, another_state) -> bool:
         """判断another_state_node状态结点的状态是否于本状态相同"""
-        if np.array_equal(self.state_array, another_state.state_array):
-            return True
-        return False
+        if another_state is not None:
+            if np.array_equal(self.state_array, another_state.state_array):
+                return True
+            return False
+        else:
+            return False
 
     def array_is_exist_in(self, a_list):
         """
@@ -93,7 +157,20 @@ class State(object):
             if np.array_equal(self.state_array, a_state.state_array):
                 # same_state = State(a.state_array.cop())
                 return True, a_state
-        return False, State(State.ERROR_ARRAY)
+        return False, State(State.ZERO_ARRAY)
+
+    def get_inverse_num(self):
+        """
+        Returns (int): 逆序数
+        """
+        count = 0
+        xy = list(self.state_array.flatten().tolist())
+        xy.remove(0)
+        for x in range(len(xy)):
+            for y in range(x, len(xy)):
+                if xy[x] > xy[y]:
+                    count += 1
+        return count
 
     def display_array(self):
         """显示该状态的数码格局"""
@@ -125,103 +202,116 @@ class Operation(object):
         return [index[0][0], index[1][0]]
 
     @classmethod
-    def move_up(cls, current_state: State):
+    def move(cls, current_state: State, direction):
+        """
+        将当前状态的数组中的0，向direction指示的方向移动
+        Args:
+            current_state(State): 当前状态
+            direction(Direction):方向
+        Returns(State):
+        """
+        # 获得0的索引
         x, y = cls.get_zero_index_in(current_state)
-        if x != 0:  # 如果空格不在最上一行，就可以向上移动
-            temp_state = State(current_state.state_array.copy(), current_state.f, current_state.g, current_state.h)
-            temp_state.state_array[x][y], temp_state.state_array[x - 1][y] \
-                = temp_state.state_array[x - 1][y], temp_state.state_array[x][y]
-            temp_state.set_father(current_state)
-            temp_state.set_g(temp_state.g + 1)
-            return temp_state
+        temp_state = State(current_state.state_array.copy())
+
+        if direction == Direction.NO_MOVE:
+            return None
+        elif direction == Direction.UP:
+            if x != 0:  # 如果空格不在最上一行，就可以向上移动
+                temp_state.state_array[x][y], temp_state.state_array[x - 1][y] \
+                    = temp_state.state_array[x - 1][y], temp_state.state_array[x][y]
+                temp_state.set_father(current_state)
+                temp_state.set_g(current_state.g + 1)
+                temp_state.last_direction_from = Direction.DOWN
+                return temp_state
+            else:
+                return None
+        elif direction == Direction.DOWN:
+            if x != State.ORDER - 1:  # 如果空格不在最下一行，就可以向下移动
+                temp_state.state_array[x][y], temp_state.state_array[x + 1][y] \
+                    = temp_state.state_array[x + 1][y], temp_state.state_array[x][y]
+                temp_state.set_father(current_state)
+                temp_state.set_g(current_state.g + 1)
+                temp_state.last_direction_from = Direction.UP
+                return temp_state
+            else:
+                return None
+        elif direction == Direction.LEFT:
+            if y != 0: # 如果空格不在最左列，就可以往左移
+                temp_state.state_array[x][y], temp_state.state_array[x][y - 1] \
+                    = temp_state.state_array[x][y - 1], temp_state.state_array[x][y]
+                temp_state.set_father(current_state)
+                temp_state.set_g(current_state.g + 1)
+                temp_state.last_direction_from = Direction.RIGHT
+                return temp_state
+            else:
+                return None
+        elif direction == Direction.RIGHT:
+            if y != State.ORDER - 1:  # 如果空格不在最右列，就可以往右移
+                temp_state.state_array[x][y], temp_state.state_array[x][y + 1] \
+                    = temp_state.state_array[x][y + 1], temp_state.state_array[x][y]
+                temp_state.set_father(current_state)
+                temp_state.set_g(current_state.g + 1)
+                temp_state.last_direction_from = Direction.LEFT
+                return temp_state
+            else:
+                return None
         else:
+            print("错误的方向")
             return None
 
     @classmethod
-    def move_down(cls, current_state: State):
-        x, y = cls.get_zero_index_in(current_state)
-        if x != State.ORDER - 1:  # 如果空格不在最下一行，就可以向下移动
-            temp_state = State(current_state.state_array.copy(), current_state.f, current_state.g, current_state.h)
-            temp_state.state_array[x][y], temp_state.state_array[x + 1][y] \
-                = temp_state.state_array[x + 1][y], temp_state.state_array[x][y]
-            temp_state.set_father(current_state)
-            temp_state.set_g(temp_state.g + 1)
-            return temp_state
-        else:
-            return None
-
-    @classmethod
-    def move_left(cls, current_state: State):
-        x, y = cls.get_zero_index_in(current_state)
-        if y != 0:  # 如果空格不在最左一列，就可以向左移动
-            temp_state = State(current_state.state_array.copy(), current_state.f, current_state.g, current_state.h)
-            temp_state.state_array[x][y], temp_state.state_array[x][y - 1] \
-                = temp_state.state_array[x][y - 1], temp_state.state_array[x][y]
-            temp_state.set_father(current_state)
-            temp_state.set_g(temp_state.g + 1)
-            return temp_state
-        else:
-            return None
-
-    @classmethod
-    def move_right(cls, current_state: State):
-        x, y = cls.get_zero_index_in(current_state)
-        if y != State.ORDER - 1:  # 如果空格不在最右列，就可以往右移
-            temp_state = State(current_state.state_array.copy(), current_state.f, current_state.g, current_state.h)
-            temp_state.state_array[x][y], temp_state.state_array[x][y + 1] \
-                = temp_state.state_array[x][y + 1], temp_state.state_array[x][y]
-            temp_state.set_father(current_state)
-            temp_state.set_g(temp_state.g + 1)
-            return temp_state
-        else:
-            return None
-
-    @classmethod
-    def get_all_next_state(cls, current_state: State) -> list:
+    def get_all_next_state(cls, father_state: State) -> list:
         """返回current_state的所有可以扩展的状态，装在列表中返回"""
         # 子状态列表
         child_states = []  # type: List[State]
 
-        up_state = cls.move_up(current_state)
-        if up_state is not None:
-            child_states.append(up_state)
+        if father_state is None:
+            print("错误，空状态无法扩展")
+            return child_states
 
-        down_state = cls.move_down(current_state)
-        if down_state is not None:
-            child_states.append(down_state)
-
-        left_state = cls.move_left(current_state)
-        if left_state is not None:
-            child_states.append(left_state)
-
-        right_state = cls.move_right(current_state)
-        if right_state is not None:
-            child_states.append(right_state)
-
+        for direction in Direction:
+            child_state = cls.move(father_state, direction)
+            if child_state is not None:
+                if father_state.father is None:  # 如果是初始节点（父节点为空）
+                    child_states.append(child_state)
+                else:                            # 否则要判断重不重复（比如往下移动又往上移动）
+                    if direction != father_state.last_direction_from:
+                        child_states.append(child_state)
         return child_states
 
-    @staticmethod
-    def display_full_info_of(a_state):
-        """
-            state (State):
-        """
-        if a_state is not None:
-            a_state.display_info()
-        else:
-            print("this state is None")
-
+    @classmethod
+    def min_sort(cls, state_list: List[State]):
+        """把最小值和列表末尾的值互换"""
+        min_f_index = 0
+        min_f = state_list[min_f_index].f
+        for index in range(len(state_list)):
+            if state_list[index].f < min_f:
+                min_f = state_list[index].f
+                min_f_index = index
+        state_list[min_f_index], state_list[-1] = state_list[-1], state_list[min_f_index]
 
 if __name__ == "__main__":
     a = State(State.CORRECT_ARRAY)
 
     # 测试单方向移动
-    b = State(np.array([[1, 0, 2], [3, 4, 5], [6, 7, 8]]))
+    b = State(np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
     b.display_info()
-    c = Operation.move_down(b)
+    c = Operation.move(b, Direction.RIGHT)
     c.display_info()
 
     # 测试生成子状态列表
     a_child_list = Operation.get_all_next_state(a)
+    aa_child_list = Operation.get_all_next_state(a_child_list[0])
+    aaa_child_list = Operation.get_all_next_state(aa_child_list[0])
     for state in a_child_list:
-        Operation.display_full_info_of(state)
+        state.display_info()
+    for state in aa_child_list:
+        state.display_info()
 
+    # 测试计算H值
+
+    t = [1, 2, 3]
+    t[0], t[1] = t[1], t[0]
+    t[-1], t[-1] = t[-1], t[-1]
+    print(t)
